9002585fa30204ec35927c87deaa3b474ef78e5e
PrinceNanaPoku <princenanapoku64@gmail.com>
2024-05-02 19:55:45 +0000

    further commit

--- app.py
+++ app.py
@@ -12,8 +12,14 @@ print(second_head);
 user_name_1 = input('Input username: ');
 password_1 = input('Input password: ');
 
-if user_name == user_name_1 and password == password_1:
-    print('User logged in successfully');
-else:
-    print('Invalid credentials');
+try:
+
+    if user_name == user_name_1 and password == password_1:
+        print('User logged in successfully');
+    elif user_name != user_name_1 and password == password_1:
+        print('Incorrect username. Enter correct username');
+    elif user_name == user_name_1 and password != password_1:
+        print('Incorrect password. Enter correct password');
+except:
+    print('An error occurred. Restart application');
 
